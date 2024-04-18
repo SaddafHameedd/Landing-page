584a9c1b0ceb58a5ca5defed362d5274d414a9b2
saddafdev <sadafhameed46@gmail.com>
2024-04-18 23:57:40 +0500

    first commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function toggleMenu(displayState){
+  const nav=document.getElementById('header_navigation');
+  const closeIco=document.getElementById('close-ico')
+  nav.style.display=displayState;
+  nav.style.display=displayState;
+}
+
+
+
